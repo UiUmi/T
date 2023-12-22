218812015e4f1470c6b7fd2bf3e8b29d2a66736f
悦悦黄 <152991976+yueyuehuang@users.noreply.github.com>
2023-12-23 00:19:49 +0800

    图片更改 ，增加开场故事情节播放，增加游戏规则窗口
    点击小熊

--- main.py
+++ main.py
@@ -4,20 +4,18 @@ import sys
 # 初始化 Pygame
 pygame.init()
 
-
 # 游戏设置
 WIDTH, HEIGHT = 1200, 700
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("The Code Odyssey")
 
-#设置游戏窗口名称
-icon=pygame.image.load("icon\head.png")  #设置游戏窗口图标
+# 设置游戏窗口名称
+icon = pygame.image.load("icon\head.png")  # 设置游戏窗口图标
 pygame.display.set_icon(icon)
-pygame.mixer.init() #加载和播放声音
-my_sound=pygame.mixer.Sound('music\music.mp3')
-my_sound.play(-1) #无限循环播放
+pygame.mixer.init()  # 加载和播放声音
+my_sound = pygame.mixer.Sound('music\music.mp3')
+my_sound.play(-1)  # 无限循环播放
 my_sound.set_volume(0.2)
-
 
 # 加载游戏背景图像
 background_image = pygame.image.load("bgp1.png")
@@ -33,7 +31,6 @@ current_frame = 0
 player_image = run_frames[current_frame]
 player_image = pygame.transform.scale(player_image, (30, 30))
 
-
 # 加载红色爱心图像
 heart_image = pygame.image.load("heart.png")
 heart_image = pygame.transform.scale(heart_image, (30, 30))
@@ -41,7 +38,7 @@ heart_image = pygame.transform.scale(heart_image, (30, 30))
 # 设置速度和跳跃高度为 1
 player_speed = 0.6
 jump_speed = 2.5
-gravity=0.03
+gravity = 0.03
 
 # 定义主角的位置和朝向
 player_x = 50
@@ -61,7 +58,6 @@ player_health = 5
 # 主角和怪物的地面高度
 player_ground = HEIGHT - 180
 
-
 # 血量指示器位置和间距
 heart_x = 20
 heart_y = 20
@@ -70,8 +66,8 @@ coin_x = 20
 coin_y = 60
 coin_spacing = 40
 
-#主角金币数和金币图像加载
-coins=0
+# 主角金币数和金币图像加载
+coins = 0
 coins_icon = pygame.image.load("coin.png")
 coins_icon = pygame.transform.scale(coins_icon, (30, 30))
 
@@ -89,24 +85,71 @@ exit_button = pygame.transform.scale(exit_button, (200, 200))
 start_button_pos = (WIDTH // 2 - 100, HEIGHT // 2 - 40)
 exit_button_pos = (WIDTH // 2 - 100, HEIGHT // 2 + 60)
 
-
-
 # 标志，用于指示游戏是在菜单还是进行中
 in_menu = True
 
-#加载商店图标
+# 加载商店图标
 store_icon = pygame.image.load("store_icon.png")
 store_icon = pygame.transform.scale(store_icon, (100, 100))
 store_icon_pos = (WIDTH - 100, 0)
 
+# 商店背景和按钮
 store_background = pygame.image.load("store.png")
 store_background = pygame.transform.scale(store_background, (300, 500))
-
 store_close_button = pygame.image.load("store_close_button.png")
 store_close_button = pygame.transform.scale(store_close_button, (84, 63))
-store_close_button_pos=(480,530)
-# 标志，用于指示商店
-in_store=False
+store_close_button_pos = (480, 530)
+in_store = False
+
+# 故事框相关设置
+story_box_background = pygame.image.load("story_box_background.png")
+story_box_background = pygame.transform.scale(story_box_background, (600, 400))
+subtitle_font = pygame.font.SysFont(None, 30)
+subtitle_timer = pygame.time.get_ticks()
+subtitle_delay = 2000  # 2秒显示一个字幕
+story_text = [
+    "Once upon a time, in the world of algorithms and code...",
+    "You, a bright and ambitious computer science student, embarked on a journey",
+    "to explore the vast landscape of programming and software development.",
+    "",
+    "As you entered the university, you were greeted by the binary whispers of",
+    "the computers and the rhythmic hum of servers in the distance.",
+    "",
+    "Your first challenge was to unravel the mysteries of programming languages.",
+    "You dived into the realms of Python, Java, and C++, each line of code",
+    "bringing you closer to the power of turning logic into reality.",
+    "",
+    "With each passing semester, you faced new quests - algorithms, data structures,",
+    "and the enchanted world of artificial intelligence.",
+    "",
+    "Late nights were spent debugging, and the glow of your screen became a beacon",
+    "in the silent halls of the computer labs.",
+    "",
+    "You forged alliances with fellow coders, engaging in epic coding battles",
+    "and collaborative quests to conquer challenging assignments.",
+    "",
+    "As you delved deeper, you discovered the magical repositories of open source",
+    "and contributed to the collective knowledge of the programming realm.",
+    "",
+    "Now, as you stand on the brink of graduation, you reflect on the incredible",
+    "adventure that your programming journey has been.",
+    "",
+    "The code scrolls before your eyes, and you realize that this is just",
+    "the beginning of an endless quest for knowledge and innovation.",
+    "",
+    "And so, with your keyboard as your sword and your IDE as your shield,",
+    "you step into the world, ready to write your own code odyssey."
+]
+
+current_story_index = 0
+story_box_open = False
+story_box_pos = (300, 150)
+
+
+# 故事框按钮
+story_box_button = pygame.image.load("story_box_button.png")
+story_box_button = pygame.transform.scale(story_box_button, (50, 50))
+story_box_button_pos = (WIDTH - 80, store_icon_pos[1] + store_icon.get_height() + 10)  # 放置在商店图标下方
 
 # 游戏循环
 running = True
@@ -115,8 +158,7 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-            # 检测菜单中的鼠标点击
-        if event.type == pygame.MOUSEBUTTONDOWN:
+        elif event.type == pygame.MOUSEBUTTONDOWN:
             mouse_pos = pygame.mouse.get_pos()
 
             # 检查鼠标点击是否在开始按钮上
@@ -128,8 +170,21 @@ while running:
             elif exit_button_pos[0] < mouse_pos[0] < exit_button_pos[0] + 200 \
                     and exit_button_pos[1] < mouse_pos[1] < exit_button_pos[1] + 200:
                 running = False
+
+            # 检查鼠标点击是否在商店图标上
+            elif store_icon_pos[0] < mouse_pos[0] < store_icon_pos[0] + 100 \
+                    and store_icon_pos[1] < mouse_pos[1] < store_icon_pos[1] + 100:
+                in_store = not in_store
+
+            # 检查鼠标点击是否在故事框按钮上
+            elif story_box_button_pos[0] < mouse_pos[0] < story_box_button_pos[0] + 50 \
+                    and story_box_button_pos[1] < mouse_pos[1] < story_box_button_pos[1] + 50:
+                story_box_open = not story_box_open
+                if story_box_open:
+                    subtitle_timer = pygame.time.get_ticks()  # 重新设置计时器
+
     if in_menu:
-        #渲染菜单背景
+        # 渲染菜单背景
         SCREEN.blit(menu_background, (0, 0))
 
         # 渲染开始和退出按钮
@@ -137,12 +192,12 @@ while running:
         SCREEN.blit(exit_button, exit_button_pos)
 
     pygame.display.update()
-    if not in_menu:
 
+    if not in_menu:
         if in_store:
-            #渲染商店页面
+            # 渲染商店页面
             SCREEN.blit(store_background, (450, 100))
-            SCREEN.blit(store_close_button,(480,530))
+            SCREEN.blit(store_close_button, (480, 530))
 
             # 检测商店中的鼠标点击
             if event.type == pygame.MOUSEBUTTONDOWN:
@@ -152,8 +207,7 @@ while running:
                 if store_close_button_pos[0] < mouse_pos[0] < store_close_button_pos[0] + 84 \
                         and store_close_button_pos[1] < mouse_pos[1] < store_close_button_pos[1] + 63:
                     in_store = False
-                    running=True
-
+                    running = True
 
         if not in_store:
             # 角色移动
@@ -200,29 +254,44 @@ while running:
 
             # 渲染背景
             SCREEN.blit(background_image, (0, 0))
+
             # 根据朝向渲染角色
             if is_facing_right:
                 SCREEN.blit(player_image, (player_x, player_y))
             else:
                 flipped_player_image = pygame.transform.flip(player_image, True, False)
                 SCREEN.blit(flipped_player_image, (player_x, player_y))
+
             # 渲染血量指示器
             for i in range(player_health):
                 SCREEN.blit(heart_image, (heart_x + i * heart_spacing, heart_y))
 
-             # 渲染血量指示器
+            # 渲染金币指示器
             for i in range(coins):
                 SCREEN.blit(coins_icon, (coin_x + i * coin_spacing, coin_y))
-            #检测商店页面打开
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                mouse_pos = pygame.mouse.get_pos()
-                if store_icon_pos[0] < mouse_pos[0] < store_icon_pos[0] + 60 \
-                        and store_icon_pos[1] < mouse_pos[1] < store_icon_pos[1] + 60:
-                    in_store = not in_store
+
+            # 渲染商店图标
             SCREEN.blit(store_icon, store_icon_pos)
+
+            # 渲染故事框按钮
+            SCREEN.blit(story_box_button, story_box_button_pos)
+
+            # 如果故事框打开，渲染故事文本
+            if story_box_open:
+                SCREEN.blit(story_box_background, story_box_pos)
+                elapsed_time = pygame.time.get_ticks() - subtitle_timer
+                if elapsed_time < subtitle_delay * len(story_text):
+                    current_index = elapsed_time // subtitle_delay
+                    rendered_text = story_text[:current_index + 1]
+                    for i, line in enumerate(rendered_text):
+                        text_surface = subtitle_font.render(line, True, (0, 0, 0))
+                        text_rect = text_surface.get_rect(center=(story_box_pos[0] + 300, story_box_pos[1] + 50 + i * 30))
+                        SCREEN.blit(text_surface, text_rect)
+                else:
+                    # 当字幕全部显示完毕后，重新开始循环
+                    subtitle_timer = pygame.time.get_ticks()
+
             pygame.display.update()
-
-
 
 # 游戏结束
 pygame.quit()
