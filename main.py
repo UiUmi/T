ba6e735b587c83ed8613e4b964235867069ee382
UiUmi <l129211091179@163.com>
2024-01-03 23:21:05 +0800

    好了

--- main.py
+++ main.py
@@ -283,8 +283,8 @@ class LevelButton:
 level_buttons = []
 
 # 示例：创建两个关卡按钮，分别关联关卡 1 和关卡 2
-button_rect1 = pygame.Rect(550, 280, 100, 100)  # 根据实际需求设置按钮位置和大小
-button_rect2 = pygame.Rect(550, 280, 100, 100)
+button_rect1 = pygame.Rect(450,280, 100, 100)  # 根据实际需求设置按钮位置和大小
+button_rect2 = pygame.Rect(546,280, 100, 100)
 level_buttons.append(LevelButton(button_rect1, level=1))
 level_buttons.append(LevelButton(button_rect2, level=2))
 
